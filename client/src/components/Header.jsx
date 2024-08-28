c2839bb1087dbbe44fc6afda15a10164753ccca1
shreyJS <deshpandeshreyas850@gmail.com>
2024-08-29 04:15:47 +0530

    connecting to DB

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Sahand's
+          Dev
         </span>
         Blog
       </Link>
@@ -32,7 +32,7 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button outline gradientDuoTone="purpleToBlue">Sign In</Button>
         </Link>
         <Navbar.Toggle />
       </div>
