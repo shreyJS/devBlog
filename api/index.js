0415a40b2b7a034b33f56e079b00e630621017bc
shreyJS <deshpandeshreyas850@gmail.com>
2024-08-29 04:43:47 +0530

    signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -13,9 +14,11 @@ mongoose
     console.log(err);
   });
 
-
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
