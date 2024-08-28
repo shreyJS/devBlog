330008a2a85ffe639da1a94780d09b0085da8ea5
shreyJS <deshpandeshreyas850@gmail.com>
2024-08-29 05:19:01 +0530

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
-
+import Footer from './components/Footer';
 function App() {
   return (
   <BrowserRouter>
@@ -20,7 +20,7 @@ function App() {
     <Route path="/projects" element={<Projects />} />
 
   </Routes>
-
+  <Footer />
 </BrowserRouter>)
 }
 
