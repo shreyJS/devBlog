c0cfef4ce816f4cecdb816ebe26d5225525c008f
shreyJS <deshpandeshreyas850@gmail.com>
2024-08-29 03:25:05 +0530

    header component setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+
 function App() {
-  return <h1 className="text-3xl text-blue-500">cummins</h1>;
+  return (
+  <BrowserRouter>
+  <Header />
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/projects" element={<Projects />} />
+
+  </Routes>
+
+</BrowserRouter>)
 }
 
 export default App;
