cd4d1f9f5b67a796958c305d5773f190a7f1a169
shreyJS <deshpandeshreyas850@gmail.com>
2024-09-02 01:01:08 +0530

    dash posts UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
+
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 
-function Dashboard() {
+export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
   useEffect(() => {
@@ -13,17 +15,16 @@ function Dashboard() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
+    <div className='min-h-screen flex flex-col md:flex-row'>
       <div className='md:w-56'>
         {/* Sidebar */}
         <DashSidebar />
       </div>
       {/* profile... */}
       {tab === 'profile' && <DashProfile />}
+      {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
     </div>
-  )
+  );
 }
-
-export default Dashboard
