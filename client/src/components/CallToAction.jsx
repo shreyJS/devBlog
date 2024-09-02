f8a177ea013abf844b8e653c9bbe6185c5a843e6
shreyJS <deshpandeshreyas850@gmail.com>
2024-09-03 02:36:00 +0530

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,24 @@
+import { Button } from 'flowbite-react';
+
+export default function CallToAction() {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className="flex-1 justify-center flex flex-col">
+            <h2 className='text-2xl'>
+                Want to stay updated?
+            </h2>
+            <p className='text-gray-500 my-2'>
+                Subscribe to our newsletter
+            </p>
+            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+                <a href="#" rel='noopener noreferrer'>
+                    Subscribe
+                </a>
+            </Button>
+        </div>
+        <div className="p-7 flex-1">
+            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+        </div>
+    </div>
+  )
+}
